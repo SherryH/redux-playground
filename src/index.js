c22234ce710a011719f593d663a1995a6f99e9e7
SherryH <enjoy.wind7rain@gmail.com>
2017-06-11 09:53:00 +1200

    Added own createStore method

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { createStore } from 'redux';
+// import { createStore } from 'redux';
 
 let state = 0;
 
@@ -20,15 +20,46 @@ console.log('state3', state = counter(state, { type: 'INCREMENT' }));
 console.log('state4', state = counter(state, { type: 'INCREMENT' }));
 console.log('state5', state = counter(state, { type: 'INCREMENT' }));
 
+
+//create the createStore function
+const createStore = (reducer) => {
+  let state;
+  let currentListeners = [];
+
+  console.log('called');
+
+  const getState = () => {
+    return reducer(state, {type:null}); //ugly
+  };
+
+  const subscribe = (listener) => {
+    currentListeners.push(listener);
+  };
+
+  const dispatch = (action) => {
+    //an action is dispatched
+    //call the reducer to change the state
+    state = reducer(state, action);
+    //call the listener subscribe to the chage of state
+    currentListeners.forEach(listener=>listener());
+  };
+  return {
+    getState,
+    subscribe,
+    dispatch
+  };
+};
+
 const store = createStore(counter);
+console.log('state',store.getState());
 
 const render = () =>{
   document.body.innerText = store.getState();
 };
-render();
 
 console.log('get state',store.getState());
 document.addEventListener('click', () => {
   store.dispatch({type: 'INCREMENT'});
 });
 store.subscribe(render);
+render();
